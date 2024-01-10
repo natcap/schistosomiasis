a76328ab856576cdcd379c27d1cfaf26a7a4eb36
Doug <dcdenu4@gmail.com>
2024-01-10 08:39:15 -0500

    Rework for passing csv params

--- schisto-main.py
+++ schisto-main.py
@@ -12,7 +12,7 @@ import taskgraph
 from osgeo import gdal
 from osgeo import osr
 
-from src import schistosomiasis
+from src.natcap.invest import schistosomiasis
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -31,6 +31,32 @@ _SENEGAL_SRS.ImportFromEPSG(_SENEGAL_EPSG)
 UINT32_NODATA = int(numpy.iinfo(numpy.uint32).max)
 FLOAT32_NODATA = float(numpy.finfo(numpy.float32).min)
 BYTE_NODATA = 255
+
+
+def _gdal_warp(target_path, input_path, xRes=None, yRes=None):
+    """Taskgraph wrapper for gdal warp."""
+    kwargs={
+        'destNameOrDestDS': target_path,
+        'srcDSOrSrcDSTab': input_path,
+        'dstSRS': _SENEGAL_SRS,
+        'xRes': xRes,
+        'yRes': yRes,
+        }
+    gdal.Warp(**kwargs)
+
+def _pop_density_to_count(pop_density_path, target_path):
+    """ """
+    population_raster_info = pygeoprocessing.get_raster_info(pop_density_path)
+    pop_pixel_area = abs(numpy.multiply(*population_raster_info['pixel_size']))
+
+    kwargs={
+        'op': lambda x: x*pop_pixel_area,  # convert count per pixel to meters sq to hectares
+        'rasters': [pop_density_path],
+        'target_path': target_path,
+        'target_nodata': -1,
+    }
+
+    pygeoprocessing.raster_map(**kwargs)
 
 def _convert_to_from_density(source_raster_path, target_raster_path,
         direction='to_density'):
@@ -91,22 +117,20 @@ if __name__ == "__main__":
         'schistosomiasis', 'data', 'procured-data')
     output_dir = os.path.join(
         'C:', os.sep, 'Users', 'ddenu', 'Workspace', 'NatCap', 'Repositories',
-        'schistosomiasis', 'data', 'output_tests')
-    preprocess_dir = os.path.join(
-        'C:', os.sep, 'Users', 'ddenu', 'Workspace', 'NatCap', 'Repositories',
-        'schistosomiasis', 'data', 'procured-data', 'preprocessed')
+        'schistosomiasis', 'data', 'output_tests_beta')
+    preprocess_dir = os.path.join(procured_data, 'preprocessed')
     LOGGER.debug(f'Output dir: {output_dir}')
 
 
     # Project data to Senegal with linear units of meters
     raw_input_data = {}
-    raw_input_data['water_temp_dry_path'] = os.path.join(
+    raw_input_data['water_temp_dry_raster_path'] = os.path.join(
         input_data, 'sen_habsuit_waterTemp_dry_2019.tif')
-    raw_input_data['water_temp_wet_path'] = os.path.join(
+    raw_input_data['water_temp_wet_raster_path'] = os.path.join(
         input_data, 'sen_habsuit_waterTemp_wet_2019.tif')
-    raw_input_data['ndvi_dry_path'] = os.path.join(
+    raw_input_data['ndvi_dry_raster_path'] = os.path.join(
         input_data, 'sen_habsuit_NDVI_dry_2019.tif')
-    raw_input_data['ndvi_wet_path'] = os.path.join(
+    raw_input_data['ndvi_wet_raster_path'] = os.path.join(
         input_data, 'sen_habsuit_NDVI_wet_2019.tif')
     raw_input_data['water_presence_path'] = os.path.join(
         input_data, 'sen_basin_water_mask.tif')
@@ -115,6 +139,7 @@ if __name__ == "__main__":
     n_workers = -1  # Synchronous execution
     graph = taskgraph.TaskGraph(work_token_dir, n_workers)
 
+
     args = {}
     for key, path in raw_input_data.items():
         target_projected_path = os.path.join(
@@ -122,12 +147,12 @@ if __name__ == "__main__":
         args[key] = target_projected_path
 
         project_task = graph.add_task(
-            gdal.Warp,
+            _gdal_warp,
             kwargs={
-                'destNameOrDestDS': target_projected_path,
-                'srcDSOrSrcDSTab': path,
-                'dstSRS': _SENEGAL_SRS,
-                #'resolution': (30,30)
+                'target_path': target_projected_path,
+                'input_path': path,
+                'xRes': 30,
+                'yRes': 30,
             },
             target_path_list=[target_projected_path],
             task_name=f'Reproject {key} to EPSG:{_SENEGAL_EPSG}'
@@ -136,10 +161,14 @@ if __name__ == "__main__":
     # Handle population special case by converting from population count to
     # population density
     # LandScan 2020 population count from Oak Ridge National Laboratory
+    # What does the value in the LandScan datasets represent?
+    #   The value of each cell represents an estimated population count for that cell.
+    # LandScan FAQ: https://landscan.ornl.gov/about
     population_count_path = os.path.join(
         procured_data, 'sen_pd_2020_1km_UNadj.tif')
     population_density_path = os.path.join(
         preprocess_dir, 'population_density.tif')
+    # Population count to population density
     population_task = graph.add_task(
         _convert_to_from_density,
         kwargs={
@@ -148,17 +177,16 @@ if __name__ == "__main__":
             'direction':'to_density'
             },
         target_path_list=[population_density_path],
-        task_name=f'Population lat/lon to meter to density'
+        task_name=f'Population count in lat/lon to density in meter'
     )
     population_projected_path = os.path.join(
         preprocess_dir, 'population_density_projected.tif')
-
+    # Project population density raster
     project_pop_task = graph.add_task(
-        gdal.Warp,
+        _gdal_warp,
         kwargs={
-            'destNameOrDestDS': population_projected_path,
-            'srcDSOrSrcDSTab': population_density_path,
-            'dstSRS': _SENEGAL_SRS,
+            'target_path': population_projected_path,
+            'input_path': population_density_path,
         },
         target_path_list=[population_projected_path],
         dependent_task_list=[population_task],
@@ -166,37 +194,52 @@ if __name__ == "__main__":
     )
     population_proj_count_path = os.path.join(
         preprocess_dir, 'population_count_projected.tif')
-    projected_count_task = graph.add_task(
-        _convert_to_from_density,
+    # Projected population density in meters to population count
+    pop_count_task = graph.add_task(
+        func=_pop_density_to_count,
         kwargs={
-            'source_raster_path': population_projected_path,
-            'target_raster_path': population_proj_count_path,
-            'direction':'from_density'
-            },
+            'pop_density_path': population_projected_path,
+            'target_path': population_proj_count_path,
+        },
         target_path_list=[population_proj_count_path],
         dependent_task_list=[project_pop_task],
-        task_name=f'Population meter density to meter count'
-    )
+        task_name=f'Population meter density to meter count')
 
     graph.close()
     graph.join()
+    graph = None
 
-    #dem_path = os.path.join(procured_data, 'srtm.tif')
-    dem_path = os.path.join(procured_data, 'pit_filled_dem.tif')
+    # Already projected to local projection 32628 via voila web app
+    # 30 x 30 resolution (1-arcsecond, 0.000277777777778 degrees)
+    dem_path = os.path.join(procured_data, 'srtm.tif')
+    # For development and testing purposes use an already pit filled DEM
+    # Doing this because TaskGraph was NOT reading this task as already
+    # completed and was always recalculating, which was slow for development.
+    #dem_path = os.path.join(procured_data, 'pit_filled_dem.tif')
 
     args['workspace_dir'] = output_dir
     args['results_suffix'] = ""
-#    args['water_temp_dry_path'] = os.path.join(
-#        input_data, 'sen_habsuit_waterTemp_dry_2019.tif')
-#    args['water_temp_wet_path'] = os.path.join(
-#        input_data, 'sen_habsuit_waterTemp_wet_2019.tif')
-#    args['ndvi_dry_path'] = os.path.join(
-#        input_data, 'sen_habsuit_NDVI_dry_2019.tif')
-#    args['ndvi_wet_path'] = os.path.join(
-#        input_data, 'sen_habsuit_NDVI_wet_2019.tif')
-#    args['water_presence_path'] = os.path.join(
-#        input_data, 'sen_basin_water_mask.tif')
+
+    args['calc_water_distance'] = True
+    args['water_distance_func_type'] = 'exponential'
+    args['water_distance_table_path'] = os.path.join(procured_data, 'exponential-water-distance.csv')
+
+    args['calc_temperature'] = True
+    args['temperature_func_type'] = 'linear'
+    args['temperature_table_path'] = os.path.join(procured_data, 'linear-temperature.csv')
+
+    args['calc_ndvi'] = True
+    args['ndvi_func_type'] = 'default'
+    args['ndvi_table_path'] = ''
+
+    args['calc_population'] = True
+    args['population_func_type'] = 'default'
+    args['population_table_path'] = ''
     args['population_count_path'] = population_proj_count_path
+
+    args['calc_water_velocity'] = True
+    args['water_velocity_func_type'] = 'default'
+    args['water_velocity_table_path'] = ''
     args['dem_path'] = dem_path
 
     schistosomiasis.execute(args)
